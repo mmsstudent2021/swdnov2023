04c3952f14381330c6f84513b4e48377f92486a5
mmsstudent2021 <mmsstudent2021@gmail.com>
2024-03-20 03:05:06 +0700

    js update

---
+++ 4-learn-js/basic/explain.js
@@ -0,0 +1,55 @@
+// // stack
+// let  x = 5;
+// const y = 15;
+
+// // heap
+// const z = ["a","b","c"];
+// const f = function(){
+//     return "I' m f";
+// }
+
+// let x = 5;
+// console.log(x);
+
+// let y = x;
+// console.log(y);
+
+// x = 10;
+// console.log(x);
+
+// console.log(y);
+
+// const x = ["a", "b", "c"];
+// console.log(x);
+
+// const y = x;
+// console.log(y);
+
+// x[1] = "zzz";
+
+// console.log(x);
+
+// console.log(y);
+
+// let x = 5;
+// let y = 5;
+
+// console.log(x === y);
+
+// let x = [];
+// let y = [];
+
+// console.log(x === y);
+
+// const a = {};
+// const b = {};
+
+// console.log(a === b);
+
+// let x = ["a", "b", "c"];
+
+// let y = x;
+
+// x = ["d", "e", "f"];
+
+// console.log(y);
