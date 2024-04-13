450dd53e60c4380d7b337a23bd5703ae39b66f5b
mmsstudent2021 <mmsstudent2021@gmail.com>
2024-04-13 11:29:40 +0700

    oop update

--- 4-learn-js/basic/oopintro.js
+++ 4-learn-js/basic/oopintro.js
@@ -93,20 +93,197 @@
 // // console.log(x.#enginPower);
 // console.log(x.run());
 
-console.log(Date.now());
+// console.log(Date.now());
 
-const date = new Date();
+// const date = new Date();
 
-console.dir(date);
-console.log(date.getHours());
-console.log(date.getMinutes());
-console.log(date.getDate());
-console.log(date.getMonth());
-console.log(date.getFullYear());
+// console.dir(date);
+// console.log(date.getHours());
+// console.log(date.getMinutes());
+// console.log(date.getDate());
+// console.log(date.getMonth());
+// console.log(date.getFullYear());
 
-console.log(date.toDateString());
-console.log(date.toTimeString());
+// console.log(date.toDateString());
+// console.log(date.toTimeString());
 
-console.log(Math.PI);
+// console.log(Math.PI);
 
-console.log(Math.pow(2, 3));
+// console.log(Math.pow(2, 3));
+
+// class Person {
+//   constructor(name, age, skinColor) {
+//     this.name = name;
+//     this.age = age;
+//     this.skinColor = skinColor;
+//   }
+
+//   eat(){
+//     return this.name + " can eat";
+//   }
+
+//   sleep(){
+//     return this.name + " can sleep";
+//   }
+// }
+
+// class Student extends Person {
+//   constructor(name, age, skinColor, uniName, majorName) {
+//     super(name, age, skinColor);
+//     this.uniName = uniName;
+//     this.majorName = majorName;
+//   }
+// }
+
+// const p1 = new Person("Mg Mg", 15, "white");
+
+// console.log(p1);
+
+// const st1 = new Student("Mg Mg", 15, "white", "WYTU", "EP");
+
+// console.log(st1);
+
+// class A {
+//   a = "a";
+//   b = "b";
+//   c = "c";
+//   d() {
+//     return "d";
+//   }
+//   e() {
+//     return "e";
+//   }
+// }
+
+// class B extends A {
+//   x = "x";
+//   y = "y";
+//   z = "z";
+// }
+
+// const a = new A();
+// console.log(a);
+// console.log(a.d());
+// console.log(a.e());
+
+// const b = new B();
+// console.log(b);
+// console.log(b.d());
+// console.log(b.e());
+
+// class Free {
+//   repo = "unlimited";
+//   push() {}
+
+//   pull() {}
+// }
+
+// class Team extends Free {
+//   codeSpace = true;
+//   online() {}
+// }
+
+// class Enterprise extends Team {
+//   manage = "Users";
+//   defineRules() {}
+// }
+
+// const f = new Free();
+
+// console.log(f);
+
+// const t = new Team();
+// console.log(t);
+
+// const e = new Enterprise();
+// console.log(e);
+
+// class A {
+//   #x = "xxx";
+
+//   // getter , setter methods
+
+//   getX() {
+//     return this.#x;
+//   }
+
+//   setX(newValue) {
+//     this.#x = newValue;
+//   }
+// }
+
+// const a = new A();
+// console.log(a);
+// // console.log(a.#a);
+// console.log(a.getX());
+
+// a.setX("haha");
+
+// console.log(a.getX());
+
+class BankAccount {
+  #money = 0;
+
+  // getter, setter
+  // getMoney() {}
+  // setMoney() {}
+
+  transition(message) {
+    console.log(message);
+  }
+
+  // getter
+  checkBalance() {
+    this.transition("U check balance "+ this.#money);
+    return this.#money;
+  }
+
+  checkBalanceWithCurrency() {
+    return this.#money + " MMK";
+  }
+
+  // setter
+  deposit(amount) {
+    this.transition("U deposit balance "+ this.#money);
+
+    this.#money += amount;
+  }
+
+  transfer(whom, amount) {
+    if (amount < this.#money) {
+      this.#money -= amount;
+      return `Transfer money ${amount} to ${whom} successfully`;
+    } else {
+      return "Not enough money";
+    }
+  }
+
+  withdraw(amount) {
+    if (amount < this.#money) {
+      this.#money -= amount;
+      return `Withdraw money ${amount} successfully`;
+    } else {
+      return "Not enough money";
+    }
+  }
+}
+
+const ba1 = new BankAccount();
+// console.log(ba1.#money);
+// ba1.#money = 5000;
+// console.log(ba1.#money);
+
+console.log(ba1.checkBalance());
+ba1.deposit(50000);
+console.log(ba1.checkBalance());
+ba1.deposit(3000);
+console.log(ba1.checkBalance());
+ba1.deposit(2000);
+console.log(ba1.checkBalance());
+console.log(ba1.transfer("kyaw kyaw", 500));
+console.log(ba1.transfer("Mg Mg", 1500));
+console.log(ba1.checkBalance());
+console.log(ba1.withdraw(3000));
+console.log(ba1.checkBalance());
+console.log(ba1.withdraw(70000));
+console.log(ba1.checkBalanceWithCurrency());
