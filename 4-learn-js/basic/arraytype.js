02f52cf4cf989d41447b8f686613cbfde3e307b2
mmsstudent2021 <mmsstudent2021@gmail.com>
2024-04-24 23:20:56 +0700

    array functions

---
+++ 4-learn-js/basic/arraytype.js
@@ -0,0 +1,110 @@
+// console.dir(Array);
+
+// const arr = ["xxx"];
+
+// arr.push("aaa");
+// arr.push("bbb");
+// arr.push("ccc");
+
+// arr.unshift("aaa");
+// arr.unshift("bbb");
+// arr.unshift("ccc");
+
+// console.log(arr);
+
+const nums = [1, 4, 2, 5];
+// const chars = ["a", "o", "c", "e"];
+// const emojis = ["😊", "🤣"];
+
+console.log(nums);
+
+// console.log(nums.pop());
+// console.log(nums.pop());
+// console.log(nums.pop());
+
+console.log(nums.shift());
+console.log(nums.shift());
+console.log(nums.shift());
+
+console.log(nums);
+
+// const result = nums.concat(chars).concat(emojis);
+
+// const result = [...nums, ...emojis, ...chars, ...emojis];
+
+// console.log(result);
+
+// const obj = {
+//   a: "aaa",
+//   b: "bbb",
+//   c: "ccc",
+// };
+
+// const end = {
+//   x: "xxx",
+//   y: "yyy",
+//   z: "zzz",
+// };
+
+// const result = { ...obj, f: "fff", ...end, o: "ooo" };
+// console.log(result);
+
+// const fruits = [
+//   "pineapple",
+//   "orange",
+//   "grape",
+//   "strawberry",
+//   "apple",
+//   "Watermelon",
+//   "Kiwi",
+//   "veve",
+//   "ဟက်လို",
+//   true,
+//   "banana",
+//   "blueberry",
+//   123,
+//   "@mms",
+//   "mango",
+// ];
+
+// console.log(fruits);
+
+// const parts = fruits.splice(3, 5);
+// console.log(parts);
+
+// console.log(fruits);
+
+// console.log(fruits.includes("apple"));
+// console.log(fruits.includes("Kiwi"));
+// console.log(fruits.includes("hein"));
+
+// console.log(fruits.indexOf("apple"));
+// console.log(fruits.indexOf("Kiwi"));
+// console.log(fruits.indexOf("hein"));
+
+// console.log(Array.isArray([]));
+
+// const array1 = ["one", "two", "three", "four"];
+
+// console.log(array1);
+// array1.reverse();
+
+// console.log(array1);
+
+// console.log(fruits);
+
+// a-z
+// fruits.sort();
+
+// console.log(fruits);
+// console.log(fruits);
+
+// console.log(fruits.length);
+
+// console.log(fruits.toString());
+
+// console.log(fruits.join());
+
+// console.log(fruits);
+// console.log(fruits.join(" - "));
+// console.log(fruits.join("\n"));
--- 4-learn-js/basic/funtype.js
+++ 4-learn-js/basic/funtype.js
@@ -14,7 +14,7 @@
 
 // console.log(run(1, 5, 6, 6, 2, 3, 6));
 
-console.dir(Function);
+// console.dir(Function);
 
 // const run = new Function("x", "y", "return x+y");
 
@@ -22,12 +22,12 @@ console.dir(Function);
 // console.dir(run.bind(null, 1, 5).apply());
 // console.dir(run.call(null, 1, 5));
 
-function sumArg() {
-  let total = 0;
-  for (x of arguments) {
-    total += x;
-  }
-  return total;
-}
+// function sumArg() {
+//   let total = 0;
+//   for (x of arguments) {
+//     total += x;
+//   }
+//   return total;
+// }
 
-console.log(sumArg(1, 5, 2, 5,8));
+// console.log(sumArg(1, 5, 2, 5,8));
